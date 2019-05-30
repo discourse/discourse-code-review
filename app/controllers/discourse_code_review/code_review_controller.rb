f5aaa9e33c1fbe3e193980a819c1acfe62ff05b6
Daniel Waterworth <me@danielwaterworth.com>
2019-05-30 07:57:51 +0100

    Create commit notifications

--- app/controllers/discourse_code_review/code_review_controller.rb
+++ app/controllers/discourse_code_review/code_review_controller.rb
@@ -101,13 +101,50 @@ module DiscourseCodeReview
 
         DiscourseTagging.tag_topic_by_names(topic, Guardian.new(current_user), tags)
 
-        topic.add_moderator_post(
-          current_user,
-          nil,
-          bump: false,
-          post_type: Post.types[:small_action],
-          action_code: "approved"
+        old_highest_post_number = topic.highest_post_number
+        post =
+          topic.add_moderator_post(
+            current_user,
+            nil,
+            bump: false,
+            post_type: Post.types[:small_action],
+            action_code: "approved"
+          )
+
+        PostTiming.pretend_read(
+          topic.id,
+          old_highest_post_number,
+          post.post_number
         )
+
+        Notification.transaction do
+          destroyed_notifications =
+            topic.user.notifications
+              .where(
+                notification_type: Notification.types[:code_review_commit_approved],
+              )
+              .where('created_at > ?', 6.hours.ago)
+              .destroy_all
+
+          previous_approved =
+            destroyed_notifications.inject(0) do |sum, notification|
+              sum + JSON.parse(notification.data)['num_approved_commits'].to_i
+            end
+
+          if previous_approved == 0
+            topic.user.notifications.create(
+              notification_type: Notification.types[:code_review_commit_approved],
+              topic_id: topic.id,
+              post_number: post.post_number,
+              data: {num_approved_commits: 1}.to_json
+            )
+          else
+            topic.user.notifications.create(
+              notification_type: Notification.types[:code_review_commit_approved],
+              data: {num_approved_commits: previous_approved + 1}.to_json
+            )
+          end
+        end
 
         if SiteSetting.code_review_auto_unassign_on_approve && topic.user.staff?
           DiscourseEvent.trigger(:unassign_topic, topic, current_user)
