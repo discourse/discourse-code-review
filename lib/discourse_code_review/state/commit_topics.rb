discourse/discourse-code-review

commit 5fa98776e08bad30b48f1d2ede1c5327c1ffe3fb

FIX: ensures we are not attempting to store a too ling title (#57)
Before this fix a very long title would bypass validation (as we use skip_validations: true) and would then fail in the database insert as fancy_title is `character varying(400)`

```
ActiveRecord::ValueTooLong: PG::StringDataRightTruncation: ERROR:  value too long for type character varying(400)
```

lib/discourse_code_review/state/commit_topics.rb
@@ -94,10 +94,16 @@ module DiscourseCodeReview::State::CommitTopics
 
             tags << SiteSetting.code_review_commit_tag
 
+            truncated_title =
+              if Topic.fancy_title(title).length > Topic.max_fancy_title_length
+                Topic.fancy_title(title).truncate(Topic.max_fancy_title_length)
+              else
+                title
+
             post = PostCreator.create!(
               user,
               raw: raw,
-              title: title,
+              title: truncated_title,
               created_at: commit[:date],
               category: category_id,
               tags: tags,

commit 3cde20a01caed55ae26e303b70d25ab8d247b57c

FIX: syntax error introduced in 5fa9877

lib/discourse_code_review/state/commit_topics.rb
@@ -99,6 +99,7 @@ module DiscourseCodeReview::State::CommitTopics
                 Topic.fancy_title(title).truncate(Topic.max_fancy_title_length)
               else
                 title
+              end
 
             post = PostCreator.create!(
               user,