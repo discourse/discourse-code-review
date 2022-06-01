4afb4c283bb1d7eeba9686111f42bb7a40e3d6df
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2022-06-01 11:02:41 -0500

    DEV: Remove 'htmlSafe' string prototype extensions (#136)
    Context: https://deprecations.emberjs.com/v3.x/#toc_ember-string-prototype_extensions

--- assets/javascripts/discourse/components/review-topic-list-item.js
+++ assets/javascripts/discourse/components/review-topic-list-item.js
@@ -1,13 +1,14 @@
 import { findRawTemplate } from "discourse-common/lib/raw-templates";
 import { observes } from "discourse-common/utils/decorators";
 import TopicListItem from "discourse/components/topic-list-item";
+import { htmlSafe } from "@ember/template";
 
 export default TopicListItem.extend({
   @observes("topic.pinned")
   renderTopicListItem() {
     const template = findRawTemplate("list/review-topic-list-item");
     if (template) {
-      this.set("topicListItemContents", template(this).htmlSafe());
+      this.set("topicListItemContents", htmlSafe(template(this)));
     }
   },
 });
