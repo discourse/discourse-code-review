7f4a3efbce88ba1b6dd50a1455f81ff6a54081d9
Bianca Nenciu <nenciu.bianca@gmail.com>
2022-08-25 20:58:33 +0300

    DEV: Move commit check in each job

--- app/jobs/scheduled/code_review_sync_repos.rb
+++ app/jobs/scheduled/code_review_sync_repos.rb
@@ -5,20 +5,13 @@ module Jobs
     every 1.hour
 
     def execute(args = {})
-      client = DiscourseCodeReview.octokit_bot_client
-      github_commit_querier = DiscourseCodeReview.github_commit_querier
-
-      DiscourseCodeReview::State::GithubRepoCategories.each_repo_name do |repo_name|
-        github_repo = DiscourseCodeReview::GithubRepo.new(repo_name, client, github_commit_querier)
-        octokit_repo = client.repository(repo_name)
-        branch = client.branch(repo_name, octokit_repo.default_branch)
-
-        last_local_commit = github_repo.last_local_commit
-        last_remote_commit = branch.commit.sha
-
-        if last_local_commit != last_remote_commit
-          ::Jobs.enqueue(:code_review_sync_commits, repo_name: repo_name)
-        end
+      DiscourseCodeReview::GithubRepoCategory.pluck(:name, :repo_id).each do |repo_name, repo_id|
+        ::Jobs.enqueue(
+          :code_review_sync_commits,
+          repo_name: repo_name,
+          repo_id: repo_id,
+          skip_if_updated: true
+        )
       end
     end
   end
--- spec/jobs/code_review_sync_repos_spec.rb
+++ spec/jobs/code_review_sync_repos_spec.rb
@@ -4,43 +4,11 @@ require 'rails_helper'
 require_relative '../helpers/integration'
 
 describe Jobs::CodeReviewSyncRepos, type: :code_review_integration do
-  before do
-    SiteSetting.code_review_github_token = 'code_review_github_token'
-
-    declare_github_repo!(
-      owner: 'discourse',
-      repo: 'discourse-code-review',
-      default_branch: 'main',
-      last_commit: 'abcdef',
-    ) do |repo|
-      msg = 'Initial commit'
-
-      Dir.chdir(repo.workdir) do
-        File.write('README.md', <<~EOF)
-          Just store text files with your todo list items.
-        EOF
-
-        `git add README.md`
-        `git commit -m 'Initial commit'`
-        `git branch -m main`
-
-        commit = `git rev-parse HEAD`
-      end
-    end
-  end
-
-  it 'schedules sync jobs for outdated repos' do
-    DiscourseCodeReview::State::GithubRepoCategories.ensure_category(repo_name: 'discourse/discourse-code-review', repo_id: 42)
+  it 'schedules sync jobs for all repos' do
+    DiscourseCodeReview::State::GithubRepoCategories.ensure_category(repo_name: 'discourse/discourse', repo_id: 42)
+    DiscourseCodeReview::State::GithubRepoCategories.ensure_category(repo_name: 'discourse/discourse-code-review', repo_id: 43)
 
     expect { described_class.new.execute }
-      .to change { Jobs::CodeReviewSyncCommits.jobs.count }.by(1)
-  end
-
-  it 'does not schedule sync jobs for updated repos' do
-    DiscourseCodeReview::State::GithubRepoCategories.ensure_category(repo_name: 'discourse/discourse-code-review', repo_id: 42)
-    PluginStore.set(DiscourseCodeReview::PluginName, DiscourseCodeReview::GithubRepo::LAST_COMMIT + 'discourse/discourse-code-review', 'abcdef')
-
-    expect { described_class.new.execute }
-      .to change { Jobs::CodeReviewSyncCommits.jobs.count }.by(0)
+      .to change { Jobs::CodeReviewSyncCommits.jobs.count }.by(2)
   end
 end
