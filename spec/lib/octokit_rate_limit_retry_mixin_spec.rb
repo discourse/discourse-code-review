1aef50d218a2d27a7046b9f1ee1bb6cb1113a658
Bianca Nenciu <nbianca@users.noreply.github.com>
2022-11-15 22:36:57 +0200

    DEV: Fix flaky test

--- spec/lib/octokit_rate_limit_retry_mixin_spec.rb
+++ spec/lib/octokit_rate_limit_retry_mixin_spec.rb
@@ -7,6 +7,8 @@ describe OctokitRateLimitRetryMixin do
     class TestJob < ::Jobs::Base
       include OctokitRateLimitRetryMixin
     end
+    
+    let(:now) { Time.zone.now }
 
     let(:exception) { Jobs::HandledExceptionWrapper.new(Octokit::TooManyRequests.new) }
 
@@ -15,21 +17,27 @@ describe OctokitRateLimitRetryMixin do
         .to_return(status: 200, body: "", headers: {
           "X-RateLimit-Limit" => 1000,
           "X-RateLimit-Remaining" => 0,
-          "X-RateLimit-Reset" => 60.minutes.from_now.to_i,
+          "X-RateLimit-Reset" => (now + 60.minutes).to_i,
           "X-RateLimit-Resource" => "core",
           "X-RateLimit-Used" => 0,
         })
     end
 
     it 'retries after rate limit expires' do
+      freeze_time now
+
       expect(TestJob.sidekiq_retry_in_block.call(0, exception)).to be >= 3600
     end
 
     it 'retries after rate limit expires again' do
+      freeze_time now
+
       expect(TestJob.sidekiq_retry_in_block.call(1, exception)).to be >= 3600
     end
 
     it 'retries once for other errors' do
+      freeze_time now
+
       expect(TestJob.sidekiq_retry_in_block.call(0, nil)).to be >= 30
       expect(TestJob.sidekiq_retry_in_block.call(0, nil)).to be <= 90
       expect(TestJob.sidekiq_retry_in_block.call(1, nil)).to eq(:discard)
