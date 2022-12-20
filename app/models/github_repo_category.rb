d16c46a79254f0b6483275614cbc19c49b810239
Ted Johansson <drenmi@gmail.com>
2022-12-20 13:07:06 +0800

    FIX: Stop trying to sync repositories that have moved (#166)
    We currently iterate over known GithubRepoCategory records and schedule
    synchronization for each.
    
    When a repo is renamed, the old GithubRepoCategory remains, albeit with
    a repo_id that is null, alongside a new one. This causes a problem, as
    when we try to synchronise it, the repo isn't there.
    
    When a repo is transferred, the old GithubRepoCategory also remains,
    causing the same problem.
    
    This change fixes both cases by skipping repos that have been moved
    when scheduling the synchronisation.

--- app/models/github_repo_category.rb
+++ app/models/github_repo_category.rb
@@ -3,4 +3,8 @@
 class DiscourseCodeReview::GithubRepoCategory < ActiveRecord::Base
   self.table_name = 'code_review_github_repos'
   belongs_to :category
+
+  # "Moved" indicates that the repo has either been transferred or renamed.
+  #
+  scope :not_moved, -> { where.not(repo_id: nil) }
 end
