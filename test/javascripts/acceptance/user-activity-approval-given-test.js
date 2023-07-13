a2662da1a0c0cfcdb72dfae172e1c18235c7a5a2
Renato Atilio <renato@discourse.org>
2023-07-13 17:53:45 -0300

    UX: separate empty states on own and others' profile pages

--- test/javascripts/acceptance/user-activity-approval-given-test.js
+++ test/javascripts/acceptance/user-activity-approval-given-test.js
@@ -5,6 +5,7 @@ import I18n from "I18n";
 
 acceptance("User Activity / Approval Given - empty state", function (needs) {
   const currentUser = "eviltrout";
+  const anotherUser = "charlie";
   needs.user();
 
   needs.pretender((server, helper) => {
@@ -13,13 +14,27 @@ acceptance("User Activity / Approval Given - empty state", function (needs) {
     server.get(`/topics/approval-given/${currentUser}.json`, () => {
       return helper.response(emptyResponse);
     });
+
+    server.get(`/topics/approval-given/${anotherUser}.json`, () => {
+      return helper.response(emptyResponse);
+    });
   });
 
-  test("Shows a blank page placeholder", async function (assert) {
+  test("Shows a blank page placeholder on own page", async function (assert) {
     await visit(`/u/${currentUser}/activity/approval-given`);
     assert.equal(
       query("div.empty-state span.empty-state-title").innerText,
       I18n.t("code_review.approval_given_empty_state_title")
+    );
+  });
+
+  test("Shows a blank page placeholder on others' page", async function (assert) {
+    await visit(`/u/${anotherUser}/activity/approval-given`);
+    assert.equal(
+      query("div.empty-state span.empty-state-title").innerText,
+      I18n.t("code_review.approval_given_empty_state_title_others", {
+        username: anotherUser,
+      })
     );
   });
 });
