6d2da9a2a14a15522657b8f977c569ab555d0415
Mark VanLandingham <markvanlan@gmail.com>
2026-06-15 14:43:37 -0500

    FIX: Moderators can configure GitHub webhooks with the bot token

--- app/controllers/discourse_code_review/repos_controller.rb
+++ app/controllers/discourse_code_review/repos_controller.rb
@@ -4,6 +4,7 @@ module DiscourseCodeReview
   class ReposController < ::ApplicationController
     requires_plugin DiscourseCodeReview::PLUGIN_NAME
 
+    before_action :ensure_admin
     before_action :set_organization
     before_action :set_repo, only: %i[has_configured_webhook configure_webhook]
 
